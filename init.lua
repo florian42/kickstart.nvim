37b0917116ffe6c4fdb247283f42ff7cfa2a1b1a
Nazar <63452145+Tokarak@users.noreply.github.com>
2024-01-29 18:52:50 +0000

    Add Onedark Style (#590)
    * Add style options to Onedark setup (init.lua)
    
    * stylua init.lua
    
    * Load onedark through `require`
    
    * Improve commenting

--- init.lua
+++ init.lua
@@ -194,6 +194,7 @@ require('lazy').setup({
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
+    lazy = false,
     config = function()
       require('catppuccin').setup {
         integrations = {
