ee1c5e43265aadb4ca69e713dbf514878626e344
Florian Aumeier <hey@flo.fish>
2024-02-18 18:54:16 +0100

    autoclose braces and auto rename tags

---
+++ lua/custom/plugins/autotag.lua
@@ -0,0 +1,22 @@
+return {
+  'windwp/nvim-ts-autotag',
+  opts = {
+    -- Removed markdown due to errors
+    filetypes = {
+      'glimmer',
+      'handlebars',
+      'hbs',
+      'html',
+      'javascript',
+      'javascriptreact',
+      'jsx',
+      'rescript',
+      'svelte',
+      'tsx',
+      'typescript',
+      'typescriptreact',
+      'vue',
+      'xml',
+    },
+  },
+}
