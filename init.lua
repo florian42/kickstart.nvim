988d17c938f641c59b13eaae0e1a3e6297aa0d64
Florian Aumeier <hey@flo.fish>
2024-03-02 10:23:17 +0100

    mini jump and cursorword

--- init.lua
+++ init.lua
@@ -344,7 +344,7 @@ require('lazy').setup {
       -- Useful for getting pretty icons, but requires special font.
       --  If you already have a Nerd Font, or terminal set up with fallback fonts
       --  you can enable this
-      -- { 'nvim-tree/nvim-web-devicons' }
+      { 'nvim-tree/nvim-web-devicons' },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -809,6 +809,8 @@ require('lazy').setup {
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
+      require('mini.jump').setup() -- Jump to next/previous single character
+      require('mini.cursorword').setup() -- Automatic highlighting of word under cursor
     end,
   },
 
