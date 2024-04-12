4be8e908ce23408ea067f64c5b4c0475760326af
Florian Aumeier <hey@flo.fish>
2024-04-12 10:21:46 +0200

    update all plugins to latest

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -1,18 +1,19 @@
 return {
-  "nvim-neotest/neotest",
+  'nvim-neotest/neotest',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-neotest/neotest-python",
-    'nvim-neotest/neotest-jest'
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-neotest/neotest-python',
+    'nvim-neotest/neotest-jest',
+    'nvim-neotest/nvim-nio',
   },
   config = function()
-    require("neotest").setup({
+    require('neotest').setup {
       adapters = {
-        require("neotest-jest"),
-        require("neotest-python"),
+        require 'neotest-jest',
+        require 'neotest-python',
       },
-    })
+    }
   end,
 }
