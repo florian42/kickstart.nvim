a56782f4aeddda217e8f95f79e63700fb00cf5e0
Florian Aumeier <hey@flo.fish>
2024-11-12 10:33:04 +0100

    update octo config

--- lua/custom/plugins/github.lua
+++ lua/custom/plugins/github.lua
@@ -7,7 +7,11 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
+    vim.treesitter.language.register('markdown', 'octo')
     require('octo').setup {
+      suppress_missing_scope = {
+        projects_v2 = true,
+      },
       use_local_fs = false, -- use local files on right side of reviews
       enable_builtin = false, -- shows a list of builtin actions when no action is provided
       default_remote = { 'upstream', 'origin' }, -- order to try remotes
