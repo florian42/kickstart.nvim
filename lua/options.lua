79615d8e29768b3863e5151b655bbc2cfc5995ef
Florian Aumeier <hey@flo.fish>
2024-12-04 14:01:28 +0100

    darkmode

--- lua/options.lua
+++ lua/options.lua
@@ -22,7 +22,7 @@ vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
-vim.opt.background = 'light'
+-- vim.opt.background = 'light'
 vim.cmd 'colorscheme default'
 
 -- [[ Setting options ]]
