5a5b8679f0e32014a3792ce392ce871a9e6ff95d
Florian Aumeier <hey@flo.fish>
2024-12-06 21:33:11 +0100

    configure snacks further
    Signed-off-by: Florian Aumeier <hey@flo.fish>

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,37 +1,63 @@
 return {
   'folke/snacks.nvim',
-  version = "*",
+  version = '*',
   priority = 1000,
   lazy = false,
+  ---@type snacks.Config
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     bigfile = { enabled = false },
-    notifier = { enabled = false },
+    notifier = { enabled = true, timeout = 3000 },
     quickfile = { enabled = false },
-    statuscolumn = { enabled = false },
+    statuscolumn = { enabled = true },
+    dashboard = { enabled = true },
     words = { enabled = false },
+    styles = {
+      notification = {
+        wo = { wrap = true }, -- Wrap notifications
+      },
+    },
   },
-  config = function()
-    local Snacks = require 'snacks'
-    Snacks.setup()
-    Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
+  init = function()
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'VeryLazy',
+      callback = function()
+        -- Create some toggle mappings
+        local Snacks = require 'snacks'
+        Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
+        Snacks.toggle.diagnostics():map '<leader>ud'
+      end,
+    })
   end,
   keys = {
     {
       '<leader>gg',
       function()
-        require('snacks').lazygit()
+        Snacks.lazygit()
       end,
       desc = 'Lazygit',
     },
     {
+      '<leader>gf',
+      function()
+        Snacks.lazygit.log_file()
+      end,
+      desc = 'Lazygit Current File History',
+    },
+    {
       '<c-/>',
       function()
-        require('snacks').terminal()
+        Snacks.terminal()
       end,
       desc = 'Toggle Terminal',
+    },
+    {
+      '<leader>n',
+      function()
+        Snacks.notifier.show_history()
+      end,
+      desc = 'Notification History',
     },
   },
 }
